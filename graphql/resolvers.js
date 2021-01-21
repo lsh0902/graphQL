214f736a0da643fa72b236eeeaf8a231c1b7141a
lsh <hehesi2007@ajou.ac.kr>
2021-01-22 01:11:19 +0900

    id 추가

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,6 +1,29 @@
+const SH = [
+    {
+    id = "1",
+    name : "lsh",
+    age : 13,
+    schoooool : "아주대대대"
+  },
+  {
+    id = "2",
+    name : "hwj",
+    age : 23,
+    schoooool : "벳푸국제"
+  },
+  {
+    id = "3",
+    name : "ㅇㅇㅇ",
+    age : 22,
+    schoooool : "얻ㄴ가"
+  }
+
+]
+
 const resolvers = {
   Query: {
-    name : () => "seunghwan"
+    name : () => "seunghwan",
+    people : () => SH
   }
 };
 
--- index.js
+++ index.js
@@ -3,7 +3,8 @@ import resolvers from "./graphql/resolvers"
 
 
 const server = new GraphQLServer({
-  typeDefs:"graphql/schema.graphql"
+  typeDefs:"graphql/schema.graphql",
+  resolvers
 })
 
 server.start(() => console.log("Graphql Server Running"))
