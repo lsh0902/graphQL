e72a72cf2e82cca2d57854314b6d2fc79e7006fb
lsh <hehesi2007@ajou.ac.kr>
2021-01-22 00:54:37 +0900

    third

---
+++ graphql/resolvers.js
@@ -0,0 +1,7 @@
+const resolvers = {
+  Query: {
+    name : () => "seunghwan"
+  }
+};
+
+export default resolvers;
--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("sdfifd")
+import { GraphQLServer } from "graphql-yoga";
+import resolvers from "./graphql/resolvers"
+
+
+const server = new GraphQLServer({
+  typeDefs:"graphql/schema.graphql"
+})
+
+server.start(() => console.log("Graphql Server Running"))
